e37e1d34d7caac72b110e71237c7aadd20270926
sanderscody26 <sanderscody26@gmail.com>
2022-04-14 15:02:36 -0400

    Added Game Message Functionality

--- app/game-review/ui.js
+++ app/game-review/ui.js
@@ -1,4 +1,20 @@
 'use strict'
 
-const store = require('../store.js')
+// const store = require('../store.js')
 
+const onGameCreateSuccess = function () {
+  $('#game-display').html('<p>User Created Game Review Successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onGameCreateFailure = function () {
+  $('#game-display').html('<p>Error while Creating Game Review</p>')
+
+  $('form').trigger('reset')
+}
+
+module.exports = {
+  onGameCreateSuccess,
+  onGameCreateFailure
+}
