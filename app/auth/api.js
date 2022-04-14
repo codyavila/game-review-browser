b438a7782d0884abad92bea6c7c041d9be629b8e
sanderscody26 <sanderscody26@gmail.com>
2022-04-14 10:32:32 -0400

    Completed sign-in, sign-up, and sign-out
    Finished implementing authorization functionality

--- app/auth/api.js
+++ app/auth/api.js
@@ -1,7 +1,36 @@
-'use strict'
 
-const store = require('../store')
+const store = require('../store.js')
+const config = require('../config')
 
 const signUp = function (data) {
-    
+	// console.log(store)
+	return $.ajax({
+		method: 'POST',
+		url: config.apiUrl + '/sign-up',
+		data
+	})
+}
+
+const signIn = function (data) {
+	return $.ajax({
+		method: 'POST',
+		url: config.apiUrl + '/sign-in',
+		data
+	})
+}
+
+const signOut = function () {
+	return $.ajax({
+		method: 'DELETE',
+		url: config.apiUrl + '/sign-out',
+		headers: {
+			Authorization: 'Bearer ' + store.user.token,
+		},
+	})
+}
+
+module.exports = {
+	signUp,
+	signIn,
+	signOut,
 }
---
+++ app/auth/ui.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const store = require('../store.js')
+
+const onSignUpSuccess = function () {
+	$('#auth-display').html('<p>User signed up successfully</p>')
+
+	$('form').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+	$('#auth-display').html('<p>Error while signing up</p>')
+}
+
+const onSignInSuccess = function (response) {
+	$('.sign-out').show()
+	$('#auth-display').html('<p>User signed in successfully</p>')
+
+	// reset all forms
+	$('form').trigger('reset')
+
+	store.user = response.user
+
+	// console.log(response)
+	// store data from the response in my store object
+	// store.user = response.user
+
+	// reset single form
+	$('#sign-in-form').trigger('reset')
+}
+
+const onSignInFailure = function () {
+	$('#auth-display').html('<p>Error while signing in</p>')
+}
+
+const onSignOutSuccess = function () {
+	$('#auth-display').html('<p>User signed out successfully</p>')
+}
+
+const onSignOutFailure = function () {
+	$('#auth-display').html('<p>Error while signing out</p>')
+}
+
+module.exports = {
+	onSignInSuccess,
+	onSignInFailure,
+	onSignUpFailure,
+	onSignUpSuccess,
+	onSignOutSuccess,
+	onSignOutFailure,
+}
