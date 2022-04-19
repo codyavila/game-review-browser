3f49da87c01ac7dfef6c8a1ad46f2c1334e2f848
sanderscody26 <sanderscody26@gmail.com>
2022-04-19 16:10:34 -0400

    Changed URL in Config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/game-review-website.git',
+  production: 'https://arcane-inlet-19895.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
